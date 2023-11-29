4b1b4700c41f8baadcd683a94285c50199bb1c0f
Sergio Teacher <Sergio Teacher@MiniWin>
2023-11-29 14:05:44 +0100

    Inicio del proyecto

---
+++ src/miVentana/MiVentana.java
@@ -0,0 +1,43 @@
+package miVentana;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MiVentana {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MiVentana window = new MiVentana();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MiVentana() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
