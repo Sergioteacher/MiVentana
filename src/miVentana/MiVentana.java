8ffd97b429aa2350837bdc4ba55cfc09ab4f92f2
Sergio Teacher <Sergio Teacher@MiniWin>
2023-11-29 14:22:09 +0100

    Añado 2 clases para manejar eventos.

--- src/miVentana/MiVentana.java
+++ src/miVentana/MiVentana.java
@@ -6,13 +6,25 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.event.ActionEvent;
 
+/**
+ * Clase única de todo el programa
+ * @author Sergio Teacher
+ *
+ */
 public class MiVentana {
 
 	private JFrame MiFrame;
 
 	/**
-	 * Launch the application.
+	 * Método que inicia el programa
+	 * @param args - Valores iniciales tipo String.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -44,8 +56,93 @@ public class MiVentana {
 		MiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JButton MiBoton = new JButton("Evento ...");
-		MiBoton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+				MiBoton.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		MiFrame.getContentPane().add(MiBoton, BorderLayout.CENTER);
+		MiBoton.addActionListener(new ActionListener() {
+			/**
+			 * Controlando el evento mediante
+			 * una clase anónima.
+			 * @param args - tipo  "ActionEvent"
+			 */
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("He pulsado el botón");
+			}
+		});
+		
 	}
 
+}
+
+class manejadorVentana implements WindowListener {
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+class manejadorCompomentes implements ComponentListener {
+
+	@Override
+	public void componentHidden(ComponentEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentMoved(ComponentEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentResized(ComponentEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentShown(ComponentEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
